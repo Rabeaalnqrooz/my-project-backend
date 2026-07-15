b54b24983aefc72e7367f339908df814aa458c66
rabea <julianqrooz@gmail.com>
2026-07-15 21:18:23 +0300

    speed website

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
+import compression from "compression";
 // ============================================================
 // ⚙️ تحميل متغيرات البيئة — يجب أن يكون أول شيء
 // ============================================================
@@ -41,7 +42,7 @@ app.use(
     crossOriginResourcePolicy: { policy: "cross-origin" },
   }),
 );
-
+app.use(compression());
 app.use(globalLimiter);
 
 // ─── CORS — السماح بالطلبات من الـ Frontend فقط ────────────
